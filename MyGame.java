37006748e325e07afe72f8c531b36b52f61fc339
Paulo Gaspar <paulogaspar404@hotmail.com>
2015-05-10 22:22:36 -0300

    First commit, creating window and all assets ready (i think)

---
+++ MyGame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.lang.Thread;
+
+public class MyGame{
+	public static void main(String [] args){
+		JFrame frame = new JFrame("Tetris");
+		frame.setContentPane(new Panel());
+		frame.setVisible(true);	
+		frame.setResizable(false);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ Panel.java
@@ -0,0 +1,39 @@
+import javax.swing.JPanel;
+import java.lang.Thread;
+import java.lang.Runnable;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener
+import java.awt.image.BufferedImage;
+
+@SuppresWarnings("serial")
+public class Panel extends JPanel implements Runnable,KeyListener{
+	public static final WIDTH = 800, HEIGHT = 600;
+	private boolean running;
+	private Thread thread;
+	private BufferedImage stage;
+	private Graphics2D g;
+
+	public Panel(){
+		super();
+	}
+	public void addNotify(){
+		super.addNotify();
+		if(thread == null){	
+			thread = new Thread(this);
+			thread.start();
+			System.out.println("CU");
+		}
+	}
+
+	public void run(){
+
+		
+
+
+	}
+
+}
